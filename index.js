0cb0ed5aa3293277aea26b4493e81983e0b86cd5
Qianyun Mu <msbluemu@gmail.com>
2023-05-18 15:40:18 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, ,dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, ,black, pink, ,] = farmAnimals.split(' ');
+
+console.log(moo);
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, i, v] = colors;
+const [, , , , , indg, ] = colors;
+
+console.log(indg);
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,6 +19,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const {muppetName, color, song, job, partner} = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +36,12 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+
+const {album: { theMuppetMovie : {song2, song4}}} = nestedMuppet;
+const {nestedJob, nestedPartner} = nestedMuppet;
+
+console.log(song2);
 
 // Strings
 
